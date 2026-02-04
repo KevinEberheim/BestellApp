5ad4d6490b5e55a36731c0757b20ae5807d336fc
KevinEberheim <kevin.eberheim@gmail.com>
2026-02-04 17:39:23 +0100

    add Basket functions like renderBasket, renderPrices, increaseAmount, decreaseAmount and removeFromBasket
    change basket style

--- script.js
+++ script.js
@@ -1,106 +1,180 @@
 let myDishes = {
     "Burger": [
-    {
-        "Image" : "./assets/img/Burger_1.png",
-        "name" : "Veggie mushroom black burger",
-        "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-        "price": 16.90
-    },
-    {
-        "Image" : "./assets/img/Burger_2.png",
-        "name" : "All meat Burger",
-        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-        "price": 15.90
-    },
-    {
-        "Image" : "./assets/img/Burger_3.png",
-        "name" : "Beef red Burger",
-        "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-        "price": 14.90
-    },
-    {
-        "Image" : "./assets/img/Burger_4.png",
-        "name" : "Big chicken burger",
-        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-        "price": 15.90
-    }
+        {
+            "Image": "./assets/img/Burger_1.png",
+            "name": "Veggie mushroom black burger",
+            "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "price": 16.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Burger_2.png",
+            "name": "All meat Burger",
+            "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "price": 15.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Burger_3.png",
+            "name": "Beef red Burger",
+            "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "price": 14.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Burger_4.png",
+            "name": "Big chicken burger",
+            "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+            "price": 15.90,
+            "amount": 1
+        }
     ],
 
     "Pizza": [
-    {
-        "Image" : "./assets/img/Pizza_1.png",
-        "name" : "Pizza Margherita",
-        "description": "Tomato Sauce, Mozzarella",
-        "price": 11.90
-    },
-    {
-        "Image" : "./assets/img/Pizza_2.png",
-        "name" : "Pizza Chorizo",
-        "description": "Tomato slices, Mozzarella, Chorizo",
-        "price": 13.90
-    },
-    {
-        "Image" : "./assets/img/Pizza_3.png",
-        "name" : "Pizza Funghi",
-        "description": "Red onion, Olives, Button Mashrooms, Mozzarella",
-        "price": 12.90
-    },
-    {
-        "Image" : "./assets/img/Pizza_4.png",
-        "name" : "Pizza Quattro Formaggi with Chicken",
-        "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-        "price": 15.90
-    }
+        {
+            "Image": "./assets/img/Pizza_1.png",
+            "name": "Pizza Margherita",
+            "description": "Tomato Sauce, Mozzarella",
+            "price": 11.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Pizza_2.png",
+            "name": "Pizza Chorizo",
+            "description": "Tomato slices, Mozzarella, Chorizo",
+            "price": 13.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Pizza_3.png",
+            "name": "Pizza Funghi",
+            "description": "Red onion, Olives, Button Mashrooms, Mozzarella",
+            "price": 12.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Pizza_4.png",
+            "name": "Pizza Quattro Formaggi with Chicken",
+            "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+            "price": 15.90,
+            "amount": 1
+        }
     ],
-    
+
     "Salad": [
-    {
-        "Image" : "./assets/img/Salad_1.png",
-        "name" : "Warm beef arugula salad",
-        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried tomatoes, Balsamic-vinegar dressing",
-        "price": 16.90
-    },
-    {
-        "Image" : "./assets/img/Salad_2.png",
-        "name" : "Mini green Salad",
-        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
-        "price": 7.90
-    },
-    {
-        "Image" : "./assets/img/Salad_3.png",
-        "name" : "Green Salad with sea food",
-        "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-        "price": 16.90
-    },
-    {
-        "Image" : "./assets/img/Salad_4.png",
-        "name" : "Vegan green salad with tofu",
-        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-        "price": 14.90
-    }
+        {
+            "Image": "./assets/img/Salad_1.png",
+            "name": "Warm beef arugula salad",
+            "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried tomatoes, Balsamic-vinegar dressing",
+            "price": 16.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Salad_2.png",
+            "name": "Mini green Salad",
+            "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+            "price": 7.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Salad_3.png",
+            "name": "Green Salad with sea food",
+            "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "price": 16.90,
+            "amount": 1
+        },
+        {
+            "Image": "./assets/img/Salad_4.png",
+            "name": "Vegan green salad with tofu",
+            "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "price": 14.90,
+            "amount": 1
+        }
     ],
 }
 
-console.log(myDishes.Pizza[1].name);
-console.log(myDishes.Salad[2].Image);
+let myBasketDishes=[];
 
-
-function renderAllDishes(){
+function renderAllDishes() {
     let burgerRef = document.getElementById('burgerDishes');
     burgerRef.innerHTML = "";
     for (let indexBurger = 0; indexBurger < myDishes.Burger.length; indexBurger++) {
-        burgerRef.innerHTML += getBurgerDishes(indexBurger);        
+        burgerRef.innerHTML += getBurgerDishes(indexBurger);
     }
 
     let pizzaRef = document.getElementById('pizzaDishes');
     pizzaRef.innerHTML = "";
     for (let indexPizza = 0; indexPizza < myDishes.Pizza.length; indexPizza++) {
-        pizzaRef.innerHTML += getPizzaDishes(indexPizza);        
+        pizzaRef.innerHTML += getPizzaDishes(indexPizza);
     }
 
     let saladRef = document.getElementById('saladDishes');
     saladRef.innerHTML = "";
-    for (let indexSalad = 0; indexSalad < myDishes.Pizza.length; indexSalad++) {
-        saladRef.innerHTML += getSaladDishes(indexSalad);        
+    for (let indexSalad = 0; indexSalad < myDishes.Salad.length; indexSalad++) {
+        saladRef.innerHTML += getSaladDishes(indexSalad);
     }
+}
+
+function pushToBasket(index, categoryName) {
+    let dishes = myDishes[categoryName][index];
+
+    let existingIndex = myBasketDishes.findIndex(item => item.name === dishes.name);
+       
+    if (existingIndex !== -1){
+        myBasketDishes[existingIndex].amount++;
+    }
+    else{
+        myBasketDishes.push({...dishes, category: categoryName});
+    }    
+
+    renderBasket();
+}
+
+function renderBasket(){
+    let basketRef = document.getElementById('basket');
+    basketRef.innerHTML = "";
+
+    for (let i = 0; i < myBasketDishes.length; i++) {
+        basketRef.innerHTML += getBasketDishes(i);
+    }
+
+    renderPrices()
+}
+
+function increaseAmount(index){
+    myBasketDishes[index].amount++;
+    renderBasket();
+}
+
+function decreaseAmount(index){
+    if (myBasketDishes[index].amount > 1) {
+        myBasketDishes[index].amount--;
+    } else {
+        myBasketDishes.splice(index, 1);
+    }
+    renderBasket();
+}
+
+function removeFromBasket(index) {
+    myBasketDishes[index].amount = 1;
+    myBasketDishes.splice(index, 1);
+    renderBasket();
+}
+
+function renderPrices(){
+    let subTotalRef = document.getElementById('subtotal')
+    let totalRef = document.getElementById('total')
+    let deliveryFeeRef = document.getElementById('deliveryFee')
+    let subtotal = 0
+    let deliveryFee = 4.99
+
+    for (let dish of myBasketDishes) {
+        subtotal += dish.price * dish.amount;
+    }
+
+    let total = subtotal + deliveryFee;
+
+    subTotalRef.innerHTML = `${subtotal.toFixed(2)}€`;
+    deliveryFeeRef.innerHTML = `${deliveryFee.toFixed(2)}€`;
+    totalRef.innerHTML = `${total.toFixed(2)}€`;
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,41 +1,55 @@
-function getBurgerDishes(index) {
+function getBurgerDishes(indexBurger) {
     return ` <div class="dishes">
-                <img src="${myDishes.Burger[index].Image}" alt="Burger${index}.png">
+                <img src="${myDishes.Burger[indexBurger].Image}" alt="Burger${indexBurger}.png">
                 <div class="descriptionDishes">
-                    <h3>${myDishes.Burger[index].name}</h3>
-                    <span>${myDishes.Burger[index].description}</span>
+                    <h3>${myDishes.Burger[indexBurger].name}</h3>
+                    <span>${myDishes.Burger[indexBurger].description}</span>
                 </div>
                 <div class="priceDishes">
-                    <h3>${myDishes.Burger[index].price.toFixed(2)}€</h3>
-                    <button>Add to basket</button>
+                    <h3>${myDishes.Burger[indexBurger].price.toFixed(2)}€</h3>
+                    <button onclick="pushToBasket(${indexBurger}, 'Burger')">Add to basket</button>
                 </div>
             </div>`;
 }
 
-function getPizzaDishes(index) {
+function getPizzaDishes(indexPizza) {
     return ` <div class="dishes">
-                <img src="${myDishes.Pizza[index].Image}" alt="Pizza${index}.png">
+                <img src="${myDishes.Pizza[indexPizza].Image}" alt="Pizza${indexPizza}.png">
                 <div class="descriptionDishes">
-                    <h3>${myDishes.Pizza[index].name}</h3>
-                    <span>${myDishes.Pizza[index].description}</span>
+                    <h3>${myDishes.Pizza[indexPizza].name}</h3>
+                    <span>${myDishes.Pizza[indexPizza].description}</span>
                 </div>
                 <div class="priceDishes">
-                    <h3>${myDishes.Pizza[index].price.toFixed(2)}€</h3>
-                    <button>Add to basket</button>
+                    <h3>${myDishes.Pizza[indexPizza].price.toFixed(2)}€</h3>
+                    <button onclick="pushToBasket(${indexPizza}, 'Pizza')">Add to basket</button>
                 </div>
             </div>`;
 }
 
-function getSaladDishes(index) {
+function getSaladDishes(indexSalad) {
     return ` <div class="dishes">
-                <img src="${myDishes.Salad[index].Image}" alt="Salad${index}.png">
+                <img src="${myDishes.Salad[indexSalad].Image}" alt="Salad${indexSalad}.png">
                 <div class="descriptionDishes">
-                    <h3>${myDishes.Salad[index].name}</h3>
-                    <span>${myDishes.Salad[index].description}</span>
+                    <h3>${myDishes.Salad[indexSalad].name}</h3>
+                    <span>${myDishes.Salad[indexSalad].description}</span>
                 </div>
                 <div class="priceDishes">
-                    <h3>${myDishes.Salad[index].price.toFixed(2)}€</h3>
-                    <button>Add to basket</button>
+                    <h3>${myDishes.Salad[indexSalad].price.toFixed(2)}€</h3>
+                    <button onclick="pushToBasket(${indexSalad}, 'Salad')">Add to basket</button>
                 </div>
             </div>`;
+}
+
+function getBasketDishes(indexBasket){
+
+    return `<div class="basketDishes">
+                <h3>${myBasketDishes[indexBasket].name}</h3>
+                    <div class="basketCounter">
+                        <img src="./assets/icons/delete_2.svg" alt="delete.svg" onclick="removeFromBasket(${index})">
+                        <h3>${myBasketDishes[indexBasket].amount}</h3>
+                        <button onclick="increaseAmount(${indexBasket})">+</button>
+                    </div>
+                <h3 class="basketPrice">${(myBasketDishes[indexBasket].price * myBasketDishes[indexBasket].amount).toFixed(2)}€</h3>
+            </div> `
+
 }
