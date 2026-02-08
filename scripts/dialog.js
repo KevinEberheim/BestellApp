392d2969385a0efdc6e9727f893cb48f40b6c063
KevinEberheim <kevin.eberheim@gmail.com>
2026-02-08 15:21:38 +0100

    change display grid to display flex change dialog functions

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,9 +1,31 @@
 const dialogRef = document.getElementById("dialogOrderConfirmed");
+const sidebarRef = document.getElementById("sidebarId");
+let dialogTimeout = null;
 
 function openDialog() {
+    if (dialogTimeout){
+        clearTimeout(dialogTimeout);
+    }
+
     dialogRef.showModal();
+    sidebarRef.classList.add("invisible")
+
+    dialogTimeout = setTimeout(() => {
+            closeDialog();
+        }, 5000);
 }
 
 function closeDialog() {
+    if (dialogTimeout){
+        clearTimeout(dialogTimeout);
+        dialogTimeout = null;
+    }
+
     dialogRef.close();
+    for (let index = myBasketDishes.length-1; index > 0 ; index--) {
+        removeFromBasket(index) 
+        console.log(myBasketDishes.length);
+              
+    }
+    sidebarRef.classList.remove("invisible")
 }
