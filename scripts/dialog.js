62579134f079ff1bf02ccd04f61a8f1abfb7cec5
KevinEberheim <kevin.eberheim@gmail.com>
2026-02-11 23:38:47 +0100

    remove timeout from dialog add menu category change style of menu category

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,25 +1,14 @@
 const dialogRef = document.getElementById("dialogOrderConfirmed");
 const sidebarRef = document.getElementById("sidebarId");
-let dialogTimeout = null;
 
 function openDialog() {
-    if (dialogTimeout){
-        clearTimeout(dialogTimeout);
-    }
 
     dialogRef.showModal();
     sidebarRef.classList.add("invisible")
 
-    dialogTimeout = setTimeout(() => {
-            closeDialog();
-        }, 5000);
 }
 
 function closeDialog() {
-    if (dialogTimeout){
-        clearTimeout(dialogTimeout);
-        dialogTimeout = null;
-    }
 
     dialogRef.close();
     while (myBasketDishes.length > 0) {
